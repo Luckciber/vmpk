a5375bcae00c9f97a1c80f8bbe9832ff7d517db3
Pablo_Gatica <pablo.gatica02@gmail.com>
2026-01-04 16:47:15 -0300

    cambios en pagina de contacto (crud simple)

--- src/pages/contacto/Contacto.tsx
+++ src/pages/contacto/Contacto.tsx
@@ -2,6 +2,22 @@ const Contacto: React.FC = () => {
     return(
         <div>
             <h1>Página Contacto</h1>
+            <select name="motivo" required>
+            <option value="" disabled selected>Selecciona un motivo</option>
+            <option value="consulta">Consulta</option>
+            <option value="soporte">Soporte</option>
+            <option value="ventas">Ventas</option>
+            <option value="otro">Otro</option>
+        </select>
+        <br>
+        <input type="text" name="nombre" placeholder="Nombre O  Razon Social" required></input>
+        </br>
+        
+        <input type="text" name="correo" placeholder="Correo Solicitante Contacto" required></input>
+
+        <input type="text" name="Nombre o Razon Social" placeholder="nombre" required></input>
+
+        <input type="text" name="Asunto" placeholder="Asunto" required></input>
             <p>Contenido de Contacto</p>
         </div>
     );
